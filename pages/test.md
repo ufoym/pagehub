e84b6310964a80c3204a28221eaaf9c49554cebe
Ming <a@ufoym.com>
2013-07-28 21:07:39 +0800

    added basic flatpages site

---
+++ pages/test.md
@@ -0,0 +1,4 @@
+title: Ming
+date: 2013-07-28 12:00:00
+
+test
---
+++ settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+DEBUG = True
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_AUTO_RELOAD = True
