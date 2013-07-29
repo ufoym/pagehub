59180497d60475ce2b32c2d1883ef1f42544216a
Ming <a@ufoym.com>
2013-07-30 01:41:52 +0800

    added subfolder image processing

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
+import re, os, shutil
 
 # -----------------------------------------------------------------------------
 
@@ -23,6 +24,21 @@ def home():
 @app.route('/<path:path>/')
 def page(path):
     page = pages.get_or_404(path)
+
+    # process images in subfolder
+    root = os.path.dirname(os.path.abspath(__file__))
+    img_reg = re.compile(r'(<img .*?src=)"(.*?)"', re.I)
+    img_rep = r'\1"/static/img/%s_\2"' % page.path.replace('/', '_')
+    # copy images
+    new_dir = os.path.join(root, 'static', 'img')
+    org_dir = os.path.join(root, 'pages', *page.path.split('/')[:-1])
+    for img in img_reg.finditer(page.html):
+        img_fn = img.group(2)
+        org_fn = os.path.join(org_dir, img_fn)
+        new_fn = os.path.join(new_dir,
+            '%s_%s' % (page.path.replace('/', '_'), img_fn))
+        shutil.copy2(org_fn, new_fn)
+    page.html = img_reg.sub(img_rep, page.html)
     return render_template('page.html', page=page)
 
 # -----------------------------------------------------------------------------
--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ import os
 
 DEBUG = True
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
-FLATPAGES_EXTENSION = '.md'
+FLATPAGES_EXTENSION = '.txt'
 FLATPAGES_AUTO_RELOAD = True
